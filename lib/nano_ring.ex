ec3379f7efa71d1a08af8f3ea1df22caffec4da3
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2014-01-29 17:22:42 +0100

    fix continuation in new enumerable protocol

--- lib/nano_ring.ex
+++ lib/nano_ring.ex
@@ -13,24 +13,27 @@ defrecord LWWElemSet, add_set: HashSet.new(), rem_set: HashSet.new()  do
     LWWElemSet[add_set: Set.union(add_set1,add_set2),rem_set: Set.union(rem_set1,rem_set2)]
 
   defimpl Enumerable, for: LWWElemSet do
-    def reduce({:lwwlists,[],_},acc,_fun), do: acc
-    def reduce({:lwwlists,[{v_add,ts_add}|add_list],rem_list},acc,fun) do
+    def reduce({:lwwlists,[],_},{:cont,acc},_fun), do: {:done,acc}
+    def reduce({:lwwlists,[{v_add,ts_add}|add_list],rem_list},{:cont,acc},fun) do
       case rem_list |> Enum.find(fn {v_rem,_}->v_rem==v_add end) do
         {_,ts_rem} when ts_rem > ts_add -> reduce({:lwwlists,add_list,rem_list},acc,fun)
         _ ->reduce({:lwwlists,add_list,rem_list},fun.(v_add,acc),fun)
       end
     end
-    def reduce(LWWElemSet[add_set: add_set,rem_set: rem_set],acc,fun) do
+    def reduce(LWWElemSet[add_set: add_set,rem_set: rem_set],{:cont,acc},fun) do
       reduce({:lwwlists,
           add_set|>Enum.sort(&(&1 > &2))|>Enum.uniq(fn {v,_ts}->v end),
           rem_set|>Enum.sort(&(&1 > &2))|>Enum.uniq(fn {v,_ts}->v end)
         },acc,fun)
     end
-    def member?(set,e), do: { :ok, set |> Enum.any?(&(&1==e)) }
+    def reduce(_,{:halt,acc},_fun), do: {:halted, acc}
+    def reduce(s,{:suspend,acc},fun), do: {:suspended, acc, &reduce(s, &1, fun)}
+    def reduce(s,acc,fun), do: reduce(s,{:cont,acc},fun)
+    def member?(set,e), do: { :error, __MODULE__ }
     def count(set), do: set |> Enum.count(fn(_)->true end)
   end
   defimpl Inspect, for: LWWElemSet do
-    def inspect(set, opts), do: "LWWSet[#{set |> Enum.map(&Kernel.inspect(&1,opts)) |> Enum.join(",")}]"
+    def inspect(set, opts), do: "LWWSet[#{set |> Enum.map(&Inspect.Algebra.to_doc(&1,opts)) |> Enum.join(",")}]"
   end
   def member?(set,e), do: set |> Enum.member?(e)
   def to_list(set), do: set |> Enum.to_list
