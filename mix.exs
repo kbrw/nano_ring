bfff478fed9418321fcd8e2331af82c46c3e50dc
Laurent Picouleau <laurent@kbrwadventure>
2016-07-01 15:03:53 +0200

    using crdtex, down handled by Node.monitor/2 and up_set local to each node

--- mix.exs
+++ mix.exs
@@ -3,15 +3,15 @@ defmodule SaEautomation.Mixfile do
 
   def project do
     [ app: :nano_ring,
-      version: "0.0.2",
-      elixir: "~> 1.0.0",
-      deps: []
+      version: "0.0.3",
+      elixir: "~> 1.2",
+      deps: [{:crdtex, "0.0.1", git: "git://github.com/awetzel/crdtex"}]
     ]
   end
 
   def application do
     [ mod: { NanoRing.App,[] },
-      applications: [:iex],
+      applications: [:iex, :crdtex],
       env: [ data_dir: "./data" ] ]
   end
 end
