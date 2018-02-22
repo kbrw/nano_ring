c4eeb3981432262405a0dfbb7e3c8fd3fc8fedfc
Jean Parpaillon <jean.parpaillon@free.fr>
2018-02-22 11:06:04 +0100

    Split code into files

---
+++ lib/lww_elem_set.ex
@@ -0,0 +1,41 @@
+defmodule LWWElemSet do
+  @moduledoc """
+  Simple implementation of the state-based **CRDT** :
+  Last Writer Win by element in a Two-Phase Set
+  Write are done by simple unions of the 2 add and remove sets
+  All reads are done through a reduce function and Enum module
+  """
+  defstruct add_set: MapSet.new, rem_set: MapSet.new
+  def put(set,e), do:
+    %{set| add_set: set.add_set|>MapSet.put({e,:erlang.timestamp()})}
+  def delete(set,e), do:
+    %{set| rem_set: set.rem_set|>MapSet.put({e,:erlang.timestamp()})}
+  def union(%LWWElemSet{add_set: add_set1,rem_set: rem_set1},%LWWElemSet{add_set: add_set2,rem_set: rem_set2}), do:
+    %LWWElemSet{add_set: MapSet.union(add_set1,add_set2),rem_set: MapSet.union(rem_set1,rem_set2)}
+
+  defimpl Enumerable, for: LWWElemSet do
+    def reduce({:lwwlists,[],_},{:cont,acc},_fun), do: {:done,acc}
+    def reduce({:lwwlists,[{v_add,ts_add}|add_list],rem_list},{:cont,acc},fun) do
+      case rem_list |> Enum.find(fn {v_rem,_}->v_rem==v_add end) do
+        {_,ts_rem} when ts_rem > ts_add -> reduce({:lwwlists,add_list,rem_list},acc,fun)
+        _ ->reduce({:lwwlists,add_list,rem_list},fun.(v_add,acc),fun)
+      end
+    end
+    def reduce(%LWWElemSet{add_set: add_set,rem_set: rem_set},{:cont,acc},fun) do
+      reduce({:lwwlists,
+          add_set|>Enum.sort(&(&1 > &2))|>Enum.uniq_by(fn {v,_ts}->v end),
+          rem_set|>Enum.sort(&(&1 > &2))|>Enum.uniq_by(fn {v,_ts}->v end)
+        },acc,fun)
+    end
+    def reduce(_,{:halt,acc},_fun), do: {:halted, acc}
+    def reduce(s,{:suspend,acc},fun), do: {:suspended, acc, &reduce(s, &1, fun)}
+    def reduce(s,acc,fun), do: reduce(s,{:cont,acc},fun)
+    def member?(_s,_e), do: { :error, __MODULE__ }
+    def count(set), do: set |> Enum.count(fn(_)->true end)
+  end
+  defimpl Inspect, for: LWWElemSet do
+    def inspect(set, opts), do: "%LWWElemSet{#{set |> Enum.map(&Inspect.Algebra.to_doc(&1,opts)) |> Enum.join(",")}}"
+  end
+  def member?(set,e), do: set |> Enum.member?(e)
+  def to_list(set), do: set |> Enum.to_list
+end
---
+++ lib/nano_ring.app.ex
@@ -0,0 +1,16 @@
+defmodule NanoRing.App do
+  use Application
+  def start(_type,_args) do
+    :supervisor.start_link(NanoRing.App.Sup,[])
+  end
+  
+  defmodule Sup do
+    use Supervisor
+    def init([]) do
+      supervise([
+        worker(:gen_event,[{:local,NanoRing.Events}], id: NanoRing.Events),
+        worker(NanoRing,[])
+      ], strategy: :one_for_one)
+    end
+  end
+end
